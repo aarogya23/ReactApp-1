36eef1461ca01cd029867ad7eec7cc2f73abed5a
Aarogya <aarogyathapa375@gmail.com>
2026-06-23 21:39:53 +0545

    Update(decrease) : option to decrease the count not less then 0

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-center mt-5">
@@ -10,7 +16,6 @@ const Counter = () => {
       </h1>
 
       <div className="h-[80vh] flex flex-col justify-center items-center gap-8 text-4xl">
-        
         <p>Count: {count}</p>
 
         <button
@@ -20,15 +25,12 @@ const Counter = () => {
           Increment
         </button>
 
-<button
-          onClick={() => setCount(count - 1)}
-          className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition-colors"
+        <button
+          onClick={decrement}
+          className="bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition-colors"
         >
           Decrement
         </button>
-
-        
-
       </div>
     </div>
   );
