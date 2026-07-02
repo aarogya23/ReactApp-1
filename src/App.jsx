746ec5cd1fe663b9e3b30c7759689261aa29b505
Aarogya <aarogyathapa375@gmail.com>
2026-07-02 18:30:50 +0545

    Update(Dashboard) : simple update on the code which have html css and js

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import MyRouter from './MyRouter'
 import { MyContext, MyContextProvider } from './hooks/MyContext'
 import './App.css';
 import { Provider } from 'react-redux';
-import myStore from './redux/myStore';
+import myStore, { persistor } from './redux/myStore';
 export const MyThemeContext = createContext()
 
+// Custom PersistGate to fix React 19 + Vite compatibility issue
+const PersistGate = ({ persistor, children }) => {
+  const [isReady, setIsReady] = useState(persistor.getState().bootstrapped);
+
+  useEffect(() => {
+    const unsubscribe = persistor.subscribe(() => {
+      setIsReady(persistor.getState().bootstrapped);
+    });
+    return unsubscribe;
+  }, [persistor]);
+
+  return isReady ? children : null;
+};
 
 const App = () => {
 
@@ -26,11 +39,13 @@ const App = () => {
   return (
     <>
     <Provider store = {myStore}>
-    <MyThemeContext.Provider value={{theme, setTheme}}>
-    <MyContextProvider>
-      <MyRouter />
-    </MyContextProvider>
-    </MyThemeContext.Provider>
+      <PersistGate loading={null} persistor={persistor}>
+          <MyThemeContext.Provider value={{theme, setTheme}}>
+          <MyContextProvider>
+            <MyRouter />
+          </MyContextProvider>
+          </MyThemeContext.Provider>
+      </PersistGate>
     </Provider>
     </>
   )
--- src/redux/myStore.jsx
+++ src/redux/myStore.jsx
@@ -3,15 +3,35 @@ import counterReducer from "./counterReducer"
 import personReducer from "./personReducer"
 import { combineReducers } from "redux"
 
+import { persistStore, persistReducer } from 'redux-persist'
 
-// const myStore = createStore(personReducer)
-
+// Custom storage to bypass Vite's issues with redux-persist/lib/storage
+const customStorage = {
+    getItem: (key) => Promise.resolve(window.localStorage.getItem(key)),
+    setItem: (key, value) => {
+        window.localStorage.setItem(key, value);
+        return Promise.resolve();
+    },
+    removeItem: (key) => {
+        window.localStorage.removeItem(key);
+        return Promise.resolve();
+    }
+};
 
 const rootReducer = combineReducers({
     counter: counterReducer,
     person: personReducer
 })
 
-const myStore = createStore(rootReducer)
+const persistConfig = {
+    key: 'root',
+    storage: customStorage, // Use our bug-free custom storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const myStore = createStore(persistedReducer)
+
+export const persistor = persistStore(myStore)
 
 export default myStore
